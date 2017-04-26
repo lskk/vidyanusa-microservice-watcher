3066a66e71ff2bd8fb025f9225c8976ea28833d2
emilhamep <emilhamep@gmail.com>
2017-04-26 23:57:03 +0700

    Inisial projek

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
+
+
+router.get('/', function(req, res, next) {
+
+});
+
+
+module.exports = router;
