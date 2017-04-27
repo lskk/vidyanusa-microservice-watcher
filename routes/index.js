163e67b4c96e0a1de78868003856fd586903f08c
emilhamep <emilhamep@gmail.com>
2017-04-27 10:55:00 +0700

    Mongo oplog error di versi 2.0.2

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,39 @@ var router = express.Router();
 var app = express();
 var bodyParser = require('body-parser');
 
+//Deklarasi library untuk listen Mongo DB
+var mongoOplog = require('mongo-oplog');
+//import MongoOplog from 'mongo-oplog'
+const oplog = mongoOplog('mongodb://127.0.0.1:27017/vidyanusa', { ns: 'vidyanusa.schools' })
 
-router.get('/', function(req, res, next) {
+oplog.tail();
 
+oplog.on('op', data => {
+  console.log(data);
 });
 
+oplog.on('insert', doc => {
+  console.log(doc);
+});
+
+oplog.on('update', doc => {
+  console.log(doc);
+});
+
+oplog.on('delete', doc => {
+  console.log(doc.o._id);
+});
+
+oplog.on('error', error => {
+  console.log(error);
+});
+
+oplog.on('end', () => {
+  console.log('Stream ended');
+});
+
+oplog.stop(() => {
+  console.log('server stopped');
+});
 
 module.exports = router;
